d1987756d650ba320d76b2a4cf5cb0a5ef8bce95
Vishal Kumar <vishal02376@gmail.com>
2025-02-04 19:30:51 +0530

    Fix port issue for Render deployment

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const videoRoutes = require('./routes/videoRoutes');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;  // Use Render's assigned port
 
 // Middleware
 app.use(cors());
